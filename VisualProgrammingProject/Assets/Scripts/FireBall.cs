3e2768cb182025eab5aac83081fb7c13fc765543
Denis <47297652+Da1W@users.noreply.github.com>
2021-06-26 17:40:17 +0500

    Init

---
+++ VisualProgrammingProject/Assets/Scripts/FireBall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Bolt;
+
+public class FireBall : MonoBehaviour
+{
+    float speed = 5;
+    private Vector3 direction;
+
+    public void setDirection(Vector3 dir)
+    {
+        direction = dir;
+    }
+
+    private void Start()
+    {
+        setDirection(transform.forward);
+        Destroy(gameObject, 5f);
+    }
+
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+        speed += 0.1f;
+    }
+}
